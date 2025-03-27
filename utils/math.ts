08ccd146054bb787ed31d2f458194a128ece2200
xfontr <xifre@Mac.technicolor.net>
2025-03-27 18:33:48 +0100

    Remove lerp util

--- utils/math.ts
+++ utils/math.ts
@@ -1,19 +1,6 @@
 import type { Location } from "~/types/Location";
 
 /**
- * Linearly interpolates between two values.
- *
- * @param {number} factor - The interpolation factor (0 = start, 1 = end).
- * @returns A function that takes `start` and `end` and returns the interpolated value.
- *
- * @example
- * const lerpHalfway = lerp(0.5);
- * console.log(lerpHalfway(10, 20)); // 15
- */
-export const lerp = (factor: number) => (start: number, end: number) =>
-    start + (end - start) * factor;
-
-/**
  * Generates a random integer between a specified minimum and maximum value (inclusive).
  *
  * @returns {number} A random integer between `min` and `max` (inclusive).
