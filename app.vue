461cfe334debe9f4612a6851d603886b690db6bc
xfontr <xifre@Mac.technicolor.net>
2025-03-28 09:36:53 +0100

    Refactor layout, pages, and component

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div><NuxtPage /></div>
+    <div>
+        <NuxtLayout><NuxtPage /></NuxtLayout>
+    </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <main class="main"><slot /></main>
+</template>
+
+<style lang="scss" scoped>
+.main {
+    padding: 8rem 2rem;
+}
+</style>
