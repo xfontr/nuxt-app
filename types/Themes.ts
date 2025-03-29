cdc7a093e6fb5ad78c35bcb05a5d23957b63fe26
xfontr <xifre@Mac.technicolor.net>
2025-03-29 23:18:05 +0100

    Automatic theme compiler from css to ts

---
+++ types/Themes.ts
@@ -0,0 +1,10 @@
+export type Themes =
+    | "theme-dark-purple"
+    | "theme-black-white"
+    | "theme-dark-purple"
+    | "theme-brown"
+    | "theme-purple"
+    | "theme-lavender"
+    | "theme-blue"
+    | "theme-tomato"
+    | "theme-pink";
