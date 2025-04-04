f0c942be3e426ef9d3dc76f51c6589e50b1c6f58
xfontr <xifre@Mac.technicolor.net>
2025-04-04 22:27:17 +0200

    Consistent test configs

--- app/vitest.config.ts
+++ app/vitest.config.ts
@@ -1,8 +1,17 @@
+import { coverageConfigDefaults } from "vitest/config";
 import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
     test: {
         globals: true,
         environment: "happy-dom",
+        coverage: {
+            exclude: [
+                ...coverageConfigDefaults.exclude,
+                "**/types/**",
+                "**/configs/**",
+                "**/index.ts",
+            ],
+        },
     },
 });
