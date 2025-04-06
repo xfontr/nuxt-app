62ef8ae1f7992a2bd02fd18548b1e93adb45e999
xfontr <xifre@Mac.technicolor.net>
2025-04-06 18:19:24 +0200

    Include lcov coverage

--- configs/vitest.config.js
+++ configs/vitest.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
                 "src/types/*",
                 "src/configs/*",
             ],
+            reporter: ["text", "html", "clover", "json", "lcov"],
         },
     },
     plugins: [],
