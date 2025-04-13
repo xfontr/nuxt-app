9d0419628dc3ce6c7273bf130914adbac413b56b
xfontr <xifre@Mac.technicolor.net>
2025-04-13 12:36:12 +0200

    Add husky hooks

---
+++ configs/eslint.js
@@ -0,0 +1,10 @@
+import * as CONFIGS from "./eslint.config.js";
+import tseslint from "typescript-eslint";
+
+/**
+ * @param  {("tsConfig" | "tsTestConfig" | "tsVueConfig"| "prettierConfig")[]} configs
+ */
+const createEslintConfig = (...configs) =>
+    tseslint.config(...configs.map((config) => CONFIGS[config]));
+
+export default createEslintConfig;
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,5 +1,4 @@
 /// <reference types="vitest" />
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import dts from "vite-plugin-dts";
