56ea1378ca2be3e81c854206f0ce5ee21fcb4379
Xifré Font <xifre.font.97@gmail.com>
2025-05-01 17:04:49 +0200

    Add mock for game state

---
+++ packages/ui/src/features/game/test-utils/__mocks__/mockGame.ts
@@ -0,0 +1,40 @@
+import type { Game } from "../../types";
+
+const mockGame: Game = {
+    physics: {
+        gravity: 0.6,
+        jumpStrength: -10,
+        boostMultiplier: 1.5,
+        slowMultiplier: 0.4,
+        baseSpeed: 4,
+    },
+    score: {
+        frameToDistance: 0.03,
+        bugKilled: 50,
+        difficultyMultiplier: 0.015,
+    },
+    player: {
+        size: 40,
+        lives: 3,
+    },
+    laser: {
+        cost: 15,
+        min: 300,
+        max: 800,
+        offset: 800,
+        recoveryRate: 2,
+        maxReach: 0.4,
+        minReach: 0,
+    },
+    obstacles: {
+        minSpacing: 60,
+        difficultyIncrease: 10,
+        spacing: 120,
+        difficultyBreakpoint: 3000,
+        offset: 200,
+        thresholds: [20, 40, 80, 100],
+    },
+    assetsSrc: "./img/game",
+};
+
+export default mockGame;
