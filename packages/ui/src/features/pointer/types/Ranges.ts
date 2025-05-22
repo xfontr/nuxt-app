4bc938da8005d1aa66c80be5c50417cab72ace5d
Xifré Font <xifre.font.97@gmail.com>
2025-05-22 18:43:37 +0200

    TOUCH: Disable Pointer unused animation feature

--- packages/ui/src/features/pointer/types/Ranges.ts
+++
@@ -1,5 +0,0 @@
-export type Ranges = {
-    short: [number, number];
-    medium: [number, number];
-    long: [number, number];
-};
--- packages/ui/src/features/pointer/utils/pointer.ts
+++ packages/ui/src/features/pointer/utils/pointer.ts
@@ -1,19 +1,4 @@
-import { random } from "../../../utils/math";
-import type { Count } from "../types/Count";
 import type { Location } from "../../../types/Location";
-import type { Ranges } from "../types/Ranges";
-
-/**
- * Updates the count object by decrementing the `long` property
- * and resetting the `short` and `long` properties if necessary.
- *
- * @param {Count} count - The count object to be updated.
- * @returns {Count} A new count object with updated values.
- */
-export const getUpdatedCount = ({ long }: Count, range: Ranges): Count =>
-    long <= 0
-        ? { short: random(...range.long), long: random(...range.short) }
-        : { short: random(...range.medium), long: long - 1 };
 
 /**
  * Compares two location objects to check if their x and y values are equal.
