1d8fda3d2bbf69d840a347bc1bea29e943ffc79d
Xifré Font <xifre.font.97@gmail.com>
2025-05-22 12:06:37 +0200

    RESPONSIVE: Implement game pausing

--- packages/app/components/GameSection.vue
+++ packages/app/components/GameSection.vue
@@ -3,22 +3,32 @@ import { Game, GamePlayerLoc } from "@portfolio/ui";
 import { GAME_DATA as game } from "~/configs/game";
 
 const { t } = useI18n();
+
+const isPaused = ref<boolean>(false);
+
+const pause = (entry?: IntersectionObserverEntry) => {
+    isPaused.value = !entry?.isIntersecting;
+};
 </script>
 
 <template>
-    <section class="atf">
+    <section
+        v-intersect="{ handler: pause }"
+        class="game-section"
+    >
         <img
-            class="game-img"
+            class="game-section__img"
             src="/img/game/background-mountain.svg"
         />
 
-        <h1 class="game-title">
+        <h1 class="game-section__title">
             {{ $t("landing.title") }}
             <GamePlayerLoc />
         </h1>
 
         <Game
-            class="game"
+            class="game-section__game"
+            :is-paused
             :game
             :t
         />
@@ -26,58 +36,58 @@ const { t } = useI18n();
 </template>
 
 <style lang="scss" scoped>
-.atf {
+.game-section {
     position: relative;
     user-select: none;
-    height: 96vh;
+    height: 99vh;
     display: flex;
     align-items: flex-end;
     width: 100%;
-}
 
-.game {
-    position: absolute;
-    bottom: 1.55rem;
-    z-index: -2;
-}
-
-.game-img {
-    position: absolute;
-    height: 100vh;
-    width: 100%;
-    object-fit: cover;
-    object-position: left;
-
-    bottom: 17rem;
-
-    @media (min-width: $breakpoints-m) {
-        bottom: 13.5rem;
-    }
-}
-
-.game-title {
-    padding-left: 1rem;
-    filter: drop-shadow(10px 10px 4px rgba(0, 0, 0, 0.05));
-    max-width: 80%;
-    line-height: 45%;
-    font-size: clamp(100px, 20vw, 20vw);
-    -webkit-text-stroke: 1px $colors-primary-very-light;
-    -webkit-text-fill-color: $colors-secondary;
-    font-weight: $fonts-extra-bold;
-    letter-spacing: -0.5rem;
-    margin-bottom: 16rem;
-
-    @media (min-width: $breakpoints-m) {
-        padding-left: 2rem;
-        margin-bottom: 13rem;
+    &__game {
+        position: absolute;
+        bottom: 1.55rem;
+        z-index: -2;
     }
 
-    @media (min-width: $breakpoints-l) {
-        margin-bottom: 11rem;
+    &__img {
+        position: absolute;
+        height: 100vh;
+        width: 100%;
+        object-fit: cover;
+        object-position: left;
+
+        bottom: 17rem;
+
+        @media (min-width: $breakpoints-m) {
+            bottom: 13.5rem;
+        }
     }
 
-    @media (min-width: $breakpoints-xl) {
-        margin-bottom: 9rem;
+    &__title {
+        padding-left: 1rem;
+        filter: drop-shadow(10px 10px 4px rgba(0, 0, 0, 0.05));
+        max-width: 80%;
+        line-height: 45%;
+        font-size: clamp(100px, 20vw, 20vw);
+        -webkit-text-stroke: 1px $colors-primary-very-light;
+        -webkit-text-fill-color: $colors-secondary;
+        font-weight: $fonts-extra-bold;
+        letter-spacing: -0.5rem;
+        margin-bottom: 16rem;
+
+        @media (min-width: $breakpoints-m) {
+            padding-left: 2rem;
+            margin-bottom: 13rem;
+        }
+
+        @media (min-width: $breakpoints-l) {
+            margin-bottom: 11rem;
+        }
+
+        @media (min-width: $breakpoints-xl) {
+            margin-bottom: 9rem;
+        }
     }
 }
 </style>
